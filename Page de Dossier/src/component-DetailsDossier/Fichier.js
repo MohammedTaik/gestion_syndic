4c438b4c720acfbadf8c77ee17447442454c3d7f
taha <tahavoiceover@gmail.com>
2022-04-08 01:12:47 +0000

    rename folder

--- Page de Dossier/src/component-DetailsDossier/Fichier.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function Fichier(){
-    return<>
-  <div id="Fichie">
-           <img src="../image/paper-clip (1).png" alt="" id="logoFichie"/>
-        </div>
-    </>;
-}
-export default Fichier ;
